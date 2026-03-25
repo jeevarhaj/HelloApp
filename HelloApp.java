f5459b5e1a78dee96262a9c6668134d3ca58aa78
jeevarhaj <rhajjeeva@gmail.com>
2026-03-25 18:59:16 +0530

    Coded UC4: Multiple names support

--- HelloApp.java
+++ HelloApp.java
@@ -3,7 +3,16 @@ public class HelloApp {
         String name;
 
         if (args.length > 0) {
-            name = args[0];
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                sb.append(args[i]);
+                if (i < args.length - 1) {
+                    sb.append(", ");
+                }
+            }
+
+            name = sb.toString();
         } else {
             name = "World";
         }
