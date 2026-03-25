13145085c9e3aa71eeb61cb57c0ccfd619eb62a6
jeevarhaj <rhajjeeva@gmail.com>
2026-03-25 18:44:19 +0530

    UC3 implemented

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name;
 
         if (args.length > 0) {
-            name = args[0];
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
         } else {
-            name = "World";
+            System.out.println("Hello, World!");
         }
 
-        System.out.println("Hello, " + name + "!");
     }
 }
