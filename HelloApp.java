a9e14a9b8d71b32b43fffaeaaab89dd3716881c8
jeevarhaj <rhajjeeva@gmail.com>
2026-04-01 23:58:18 +0530

    Coded UC6 using substring to remove trailing delimiter

--- HelloApp.java
+++ HelloApp.java
@@ -3,18 +3,16 @@ public class HelloApp {
 
         String result;
 
-        // Check if no arguments
         if (args.length == 0) {
             result = "World";
         } else {
             StringBuilder sb = new StringBuilder();
 
-            // Enhanced for loop
             for (String name : args) {
                 sb.append(name).append(", ");
             }
 
-            // Remove last comma and space
+            // UC6 main idea
             result = sb.substring(0, sb.length() - 2);
         }
 
